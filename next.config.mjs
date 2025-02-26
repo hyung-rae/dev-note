2e98613aa06720dd24ccd3eee6a896d5de5702b2
우형래 <woohr1109@gmail.com>
2025-02-26 15:21:52 +0900

    ✨ Feat: Create action yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isProd ? '/hyung-rae' : '',
+  assetPrefix: isProd ? '/hyung-rae/' : '',
 }
 
 export default nextConfig
