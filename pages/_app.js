d67aab6c50d6e34cabf4f3cd2fff17a84367298e
우형래 <woohr1109@gmail.com>
2025-02-26 15:05:22 +0900

    ✨ Feat: Create Next app

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '@mantine/core/styles.css'
+import '../styles/global.css'
+
+export const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default App
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <a>test</a>
+}
+
+export default Home
