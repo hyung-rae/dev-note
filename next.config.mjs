d5e56fc7ee38ee116d16ec973ebffca2aa14aa0d
우형래 <woohr1109@gmail.com>
2025-02-26 15:26:19 +0900

    🚧 Wip: workflows 작업중

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   basePath: isProd ? '/hyung-rae' : '',
   assetPrefix: isProd ? '/hyung-rae/' : '',
 }
